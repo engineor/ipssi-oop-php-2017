376c4ac0993e0d069c0b7f43439aa23b948dcf21
Thomas Dutrion <thomas@engineor.com>
2017-12-14 17:00:34 +0100

    Ajouter un tableau static de films

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -4,10 +4,16 @@ declare(strict_types=1);
 
 namespace Application\Controller;
 
+use Application\Entity\Film;
+
 final class FilmController
 {
     public function indexAction() : string
     {
+        $films = [
+            new Film('Avengers'),
+        ];
+
         ob_start();
         include __DIR__.'/../../views/film.phtml';
         return ob_get_clean();
