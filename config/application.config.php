58db70d186e840588182a74c019e12b155c485a4
Thomas Dutrion <thomas@engineor.com>
2017-12-14 22:30:14 +0100

    Déplacement des factories dans des classes dédiées

--- config/application.config.php
+++ config/application.config.php
@@ -16,30 +16,28 @@ use Application\Router\ParseUriHelper;
 use Application\Router\Router;
 use Cinema\Controller\FilmController;
 use Cinema\Controller\ShowFilmController;
+use Cinema\Factory\FilmControllerFactory;
+use Cinema\Factory\FilmRepositoryFactory;
+use Cinema\Factory\ShowFilmControllerFactory;
 use Cinema\Repository\FilmRepository;
-use Psr\Container\ContainerInterface;
 
 return [
     'factories' => [
+        // Configuration du "framework applicatif"
         ParseUriHelper::class => ParseUriHelperFactory::class,
         Router::class => RouterFactory::class,
-        DateTimeInterface::class => DateTimeImmutableFactory::class,
-        LecturerController::class => LecturerControllerFactory::class,
-        FilmController::class => function(ContainerInterface $container) {
-            $conn = $container->get(FilmRepository::class);
-            return new FilmController($conn);
-        },
-        FilmRepository::class => function(ContainerInterface $container) {
-            $pdo = $container->get(PDO::class);
-            return new FilmRepository($pdo);
-        },
-        ShowFilmController::class => function(ContainerInterface $container) {
-            $filmRepository = $container->get(FilmRepository::class);
-            return new ShowFilmController($filmRepository);
-        },
-        IndexController::class => IndexControllerFactory::class,
-        LecturerRepository::class => LecturerRepositoryFactory::class,
         PDO::class => PdoConnectionFactory::class,
         DbConfigProvider::class => DbConfigProviderFactory::class,
+
+        // Configurations liées aux lecturers
+        DateTimeInterface::class => DateTimeImmutableFactory::class,
+        LecturerController::class => LecturerControllerFactory::class,
+        IndexController::class => IndexControllerFactory::class,
+        LecturerRepository::class => LecturerRepositoryFactory::class,
+
+        // Configurations liées auz films
+        FilmController::class => FilmControllerFactory::class,
+        ShowFilmController::class => ShowFilmControllerFactory::class,
+        FilmRepository::class => FilmRepositoryFactory::class,
     ],
 ];
